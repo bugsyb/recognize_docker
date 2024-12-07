0dbb939d3326f9d36a7d648906d6e7ae3348b4ae
bugsyb <5527773+bugsyb@users.noreply.github.com>
2024-12-07 23:57:36 +0100

    A few changes around nvidia-tensor-based build: - Adjustment to the latest version supporting cuda.11 - as required by Recognize - nvcr.io/nvidia/tensorflow:24.10-tf2-py3 - Warning added for manual selection of php version - Nextcloud requires specific version and such needs to be specified - latest working version set as default 8.2/bookworm base. PHP build needs to be based on Tensor which effectively is Ubuntu release based matching Debian/bookworm. By default PHP build is bookworm-slim - though this can't be met and locall tag overwrite is used to overcome the issue as documented - Additional changes around tags to make them more clear after the build - Switch added to allow from commandline build GPU version instead of default CPU. Earlier it required editing variable within script (placed at top config section).
    Tested fully and works on nVidia GPU based system.

--- nvidia-tensor-based/build-all.sh
+++ nvidia-tensor-based/build-all.sh
@@ -2,27 +2,42 @@
 
 set -e
 
-GIT_UPDATE_ON_EACH=false
-WEBFRONTEND=apache		# select frontend, apache or fpm, needs to be Debian base as nvidia is (Alpine is a no go - pain to get tensor on Alpine)
-PHP_BASE=8.2/bookworm		# currently adjust as default for NC 26 image 8.2/bookworm
-NC_BASE=26			# NC Base version, exact patch/release changes on git pulls
+# Pulls fresh git on each run, enable next line, to disable it, i.e. troubleshooting
+#GIT_UPDATE_ON_EACH=false
+WEBFRONTEND=fpm			# select frontend, apache or fpm, needs to be Debian base as nvidia is (Alpine is a no go - pain to get tensor on Alpine)
+PHP_BASE=8.2/bookworm		# currently adjust as default for NC 30 image 8.2/bookworm
+#PHP_BASE=8.2/bookworm		# currently adjust as default for NC 26 image 8.2/bookworm
+NC_BASE=30			# NC Base version, exact patch/release changes on git pulls
 
-OVERWRITE_NEXTCLOUD_TAGS=n
+#OVERWRITE_NEXTCLOUD_TAGS=n
+OVERWRITE_NEXTCLOUD_TAGS=y
 CUSTOM_ADDONS=custom-addons
 NEXTCLOUD_TAGS="-t local/nextcloud:latest -t local/nextcloud"
+TAG_TIME="$(date '+%Y%m%d-%H%M%S')"
 
-BUILD_GPU=true
-BUILD_CPU=false
+
+BUILD_GPU=false
+BUILD_CPU=true
+SKIP_GIT_UPDATES=false
 
 # potential adjustments, builds fine as of 2023.10.16
 TENSOR_IMAGE=nvcr.io/nvidia/tensorflow
-TENSOR_VER=22.03-tf2-py3
+TENSOR_VER=24.10-tf2-py3 # last on 22.04 ubuntu - bookworm, 24.04 is trixie
+#TENSOR_VER=22.03-tf2-py3 # bullseye - new nextcloud is bookworm
+#TENSOR_VER=23.04-tf2-py3 # first with 22.04 ubuntu - bookworm
+
 
 ## rarely needed to be touched
 PHP_REPO=https://github.com/docker-library/php
 PHP_DIR=php
 NC_REPO=https://github.com/nextcloud/docker
 NC_DIR=nextcloud
+
+TMPDIR=/tmp
+
+#####
+# local settings
+OD="${PWD}"
 
 ################################################################
 adjust_build_vars(){
@@ -47,6 +62,8 @@ process_cmdline(){
       -p|--php-version)
         if [ -n "$2" ]; then
 	    PHP_BASE="$2"
+        echo "!!! Version has to be based on what specific Nextcloud version is built FROM (Dockerfile)! Otherwise it won't use Tensorflow enabled image for GPU"
+        sleep 2
 	else 
 	    echo "No version supplied for $0, i.e. $PHP_BASE"
 	    exit 1
@@ -61,6 +78,17 @@ process_cmdline(){
 	    exit 1
 	fi
         shift 2
+        ;;
+      -g|--gpu)
+        BUILD_GPU=true
+        BUILD_CPU=false
+        echo "GPU Build selected"
+        shift 1
+        ;;
+      -s|--skip-git-updates)
+        SKIP_GIT_UPDATES=true
+        echo "GPU Build selected"
+        shift 1
         ;;
       -h|--help)
         echo "Usage: $0 [-n|--nextcloud <version-tag>] [-p|--php-version <version-tag>] [-w|--webfront <string>] [-h|--help]"
@@ -88,6 +116,7 @@ repo_latest(){
     cd "$local_dir"
     git fetch origin
     git pull origin master  # Replace 'master' with the appropriate branch name
+    cd "${OD}"
   else
     # If it doesn't exist, clone a fresh copy
     git clone "$repo_url" "$local_dir"
@@ -159,7 +188,9 @@ build_gpu(){
 #  I'll sleep for 10s.
 #######"
 
-  sleep 10
+#  sleep 10
+  NEXTCLOUD_TAGS="$NEXTCLOUD_TAGS -t local/nextcloud:latest-gpu"
+
   docker pull $TENSOR_IMAGE:$TENSOR_VER
   docker tag $TENSOR_IMAGE:$TENSOR_VER $(base_for php/$PHP_VER/Dockerfile)
   # get first tensor!!! as bookworm slim
@@ -167,20 +198,30 @@ build_gpu(){
   DOCKER_BUILDKIT=1 docker build -t php:$(php_inverse_tag_order $(tag_from_repo $PHP_VER)) -t local/php:$(php_inverse_tag_order $(tag_from_repo $PHP_VER)) ${PHP_DIR}/${PHP_VER}/.
   
   
-  
   #DOCKER_BUILDKIT=1 docker build -t nextcloud:$(tag_from_repo $NC_VER) -t nextcloud:${NC_VER_BUILD}-${##*/} ${NC_DIR}/${NC_VER}
   #DOCKER_BUILDKIT=1 docker build -t nextcloud:$(tag_from_repo $NC_VER) -t nextcloud:${NC_VER_BUILD}-${NC_VER##*/} -t local/nextcloud:${NC_VER_BUILD}-${NC_VER##*/} ${NC_DIR}/${NC_VER}
-  DOCKER_BUILDKIT=1 docker build -t local/nextcloud:$(tag_from_repo $NC_VER)-tensor -t local/nextcloud:${NC_VER_BUILD}-${NC_VER##*/}-tensor ${NC_DIR}/${NC_VER}
-  
+  DOCKER_BUILDKIT=1 docker build \
+    -t local/nextcloud:$(tag_from_repo $NC_VER)-tensor \
+    -t local/nextcloud:${NC_VER_BUILD}-${NC_VER##*/}-tensor \
+    -t local/nextcloud:latest-gpu \
+    ${NC_DIR}/${NC_VER}
   # custom
   if [ -n "$CUSTOM_ADDONS" ]; then
-      DOCKER_BUILDKIT=1 docker build  --build-arg NCBASE=local/nextcloud:${NC_VER_BUILD}-${NC_VER##*/}-tensor -t local/nextcloud:$(tag_from_repo $NC_VER)-tensor-custom \
-  				-t local/nextcloud:${NC_VER_BUILD}-${NC_VER##*/}-tensor-custom \
-  				 $(nc_custom_tags) $CUSTOM_ADDONS
+      DOCKER_BUILDKIT=1 docker build  \
+        --build-arg NCBASE=local/nextcloud:${NC_VER_BUILD}-${NC_VER##*/}-tensor\
+        -t local/nextcloud:$(tag_from_repo $NC_VER)-tensor-custom \
+  		-t local/nextcloud:${NC_VER_BUILD}-${NC_VER##*/}-tensor-custom\
+        -t local/nextcloud:${NC_VER_BUILD}-${NC_VER##*/}-tensor-custom-$TAG_TIME \
+        -t local/nextcloud:latest-gpu \
+  		$(nc_custom_tags) $CUSTOM_ADDONS
   #				-t local/nextcloud:${NC_VER%%/*}-custom \
   fi
   if [ "$OVERWRITE_NEXTCLOUD_TAGS" == "y" ]; then 
+    if [ -n "$CUSTOM_ADDONS" ]; then
+      split_and_tag local/nextcloud:${NC_VER_BUILD}-${NC_VER##*/}-tensor-custom ${NEXTCLOUD_TAGS//-t /}
+    else
       split_and_tag local/nextcloud:${NC_VER_BUILD}-${NC_VER##*/}-tensor ${NEXTCLOUD_TAGS//-t /}
+    fi
   fi
 }
 
@@ -190,7 +231,7 @@ build_cpu(){
   # build-non-gpu image
   #DOCKER_BUILDKIT=1 docker build -t nextcloud:$(tag_from_repo $NC_VER)-custom -t local/nextcloud:${NC_VER_BUILD}-${NC_VER##*/}-custom ${NC_DIR}/${NC_VER}
   DOCKER_BUILDKIT=1 docker build  --build-arg NCBASE=nextcloud:${NC_BASE}-${NC_VER##*/} -t local/nextcloud:$(tag_from_repo $NC_VER)-custom \
-  				-t local/nextcloud:${NC_VER_BUILD}-${NC_VER##*/}-custom \
+  				-t local/nextcloud:${NC_VER_BUILD}-${NC_VER##*/}-custom -t local/nextcloud:${NC_VER_BUILD}-${NC_VER##*/}-custom-$TAG_TIME \
   				 $(nc_custom_tags) $CUSTOM_ADDONS
 
   #${NC_VER_BUILD}-${NC_VER##*/}
@@ -198,12 +239,14 @@ build_cpu(){
 
 ################################################################
 
-
 process_cmdline "$@"
 
 adjust_build_vars
-repo_latest $PHP_REPO $PHP_DIR
-repo_latest $NC_REPO $NC_DIR
+
+if ! $SKIP_GIT_UPDATES; then
+  repo_latest $PHP_REPO $PHP_DIR
+  repo_latest $NC_REPO $NC_DIR
+fi
 # get exact Nextcloud version
 NC_VER_BUILD=$(nc_build_ver)
 [[ $BUILD_GPU == true ]] && build_gpu
